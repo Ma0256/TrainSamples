7caae83012d79c76e3f9d0ef13a79fc07eef4d9a
ma0256 <ma0256@technikum-wien.at>
2022-10-17 09:40:12 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+from pathlib import Path
+
+access_rrot = 'C:/Users/ma0256/prj/Sampledateien'
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
